ee832874f56a4589430d16ca8e81be02ccb50656
Vinicius Marinho <48867230+ViniciusAlexsander@users.noreply.github.com>
2022-09-18 13:49:29 -0300

    ajuste pwa

--- toferrado.js
+++ toferrado.js
@@ -3,9 +3,15 @@ let tarefas = [];
 onload = () => {
   const t = JSON.parse(localStorage.getItem("tarefas"));
   if (t) tarefas = t;
-
   mostraTarefas();
   document.querySelector("#inputNovaTarefa").oninput = monitoraCampoAdic;
+  document.querySelector("#inputAlteraTarefa").oninput = monitoraCampoAlt;
+  document.querySelector("#inputNovaTarefa").onkeypress = (e) => {
+    if (e.key == "Enter") adicionaTarefa();
+  };
+  document.querySelector("#inputAlteraTarefa").onkeypress = (e) => {
+    if (e.key == "Enter") alteraTarefa();
+  };
 
   document.querySelector("#btnAdic").onclick = () => {
     document.querySelector("#btnInc").disabled = true;
@@ -19,14 +25,14 @@ onload = () => {
   };
 
   document.querySelector("#btnCanc2").onclick = () => {
-    let campo = document.querySelector("#inputNovaTarefa");
+    let campo = document.querySelector("#inputAlteraTarefa");
     campo.value = "";
     campo.removeAttribute("data-id");
     ativa("tela1");
   };
 
   document.querySelector("#btnInc").onclick = () => {
-    adicionarTarefa();
+    adicionaTarefa();
   };
 
   document.querySelector("#btnAlt").onclick = () => {
@@ -34,13 +40,12 @@ onload = () => {
   };
 
   document.querySelector("#btnDel").onclick = () => {
-    apagarTarefa();
+    apagaTarefa();
   };
 };
 
 const mostraTarefas = () => {
   const listaDeTarefas = document.querySelector("#listaDeTarefas");
-
   listaDeTarefas.innerHTML = "";
   tarefas.forEach((t) => {
     let elemTarefa = document.createElement("li");
@@ -53,9 +58,9 @@ const mostraTarefas = () => {
       campo.setAttribute("data-id", t.id);
       campo.focus();
     };
-
     listaDeTarefas.appendChild(elemTarefa);
   });
+  document.querySelector("#estado").innerText = tarefas.length;
   if (tarefas.length > 0) {
     listaDeTarefas.classList.remove("hidden");
     document.querySelector("#blank").classList.add("hidden");
@@ -71,7 +76,7 @@ const ativa = (comp) => {
   document.querySelector("#" + comp).classList.remove("hidden");
 };
 
-const adicionarTarefa = () => {
+const adicionaTarefa = () => {
   let campo = document.querySelector("#inputNovaTarefa");
   let descricao = campo.value;
   if (descricao != "") {
@@ -79,7 +84,6 @@ const adicionarTarefa = () => {
       id: Math.random().toString().replace("0.", ""),
       descricao: descricao,
     });
-
     campo.value = "";
     ativa("tela1");
     salvaTarefas();
@@ -105,7 +109,7 @@ const alteraTarefa = () => {
   mostraTarefas();
 };
 
-const apagarTarefa = () => {
+const apagaTarefa = () => {
   let campo = document.querySelector("#inputAlteraTarefa");
   let idTarefa = campo.getAttribute("data-id");
   tarefas = tarefas.filter((t) => t.id != idTarefa);
@@ -116,8 +120,14 @@ const apagarTarefa = () => {
   mostraTarefas();
 };
 
+const monitoraCampoAlt = (e) => {
+  let botao = document.querySelector("#btnAlt");
+  if (e.target.value.length > 0) botao.disabled = false;
+  else botao.disabled = true;
+};
+
 const salvaTarefas = () => {
   localStorage.setItem("tarefas", JSON.stringify(tarefas));
 };
 
-navigator.serviceWorker.register('./toferrado-sw.js');
+navigator.serviceWorker.register("./toferrado-sw.js");
